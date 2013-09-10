ce803d8cc9fcf2953668df88c836828073a87253
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:29:59 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,23 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_Color.js',
+	'XXX_Color_Gradient.js',
+	'XXX_Color_Helpers.js',
+	'XXX_Color_Swatches.js',
+	
+	'XXX_CSS.js',
+	'XXX_CSS_Depth.js',
+	'XXX_CSS_Fixes.js',
+	'XXX_CSS_Position.js',
+	'XXX_CSS_Size.js',
+	'XXX_CSS_Visibility.js',
+	
+	'XXX_Animation.js',
+	'XXX_Animation_PropertyTransitions.js',
+	'XXX_Animation_Tweens.js'
+),
+'initialize.js');
+
+?>
